da1d3fe22651b781a7bde8155902d058ca4d5162
Anand Kumar <anandkr369@gmail.com>
2024-03-16 22:12:42 +0530

    Your commit message here

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/calculator-using-React/', // Base URL for your application when deployed
   plugins: [react()],
-})
+});
